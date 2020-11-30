b37a00056fdfda3858a1fdd1cae2b41c0d83f384
Into Almiala <into.almiala@gmail.com>
2020-11-30 23:52:01 +0200

    make user-friendlier

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-import tkinter as tk
-from PIL import Image, ImageTk
 from os import walk
 from playsound import playsound
 from random import randrange
+import tkinter as tk
+from PIL import Image, ImageTk
+
+clear = input("Would you like to clear played files? [y/N] ")
+if clear.lower() == "y":
+    with open("played", "w") as f:
+        f.write("")
+
+DIRECTORY = "recordings"
+_, _, files = next(walk(DIRECTORY))
+
+lastindex = None
+
+def play():
+    global lastindex
+    if len(files):
+        index = randrange(len(files))
+        with open("played") as f:
+            played = list(map(lambda x: x.strip(), f.readlines()))
+            while files[index] in played:
+                index = randrange(len(files))
+        playsound(f"{DIRECTORY}/{files[index]}")
+        lastindex = index
+    else:
+        print("All files played.")
+
+def getcolor(event):
+    if not lastindex is None:
+        value = img.getpixel((event.x, event.y))
+        with open("played", "a") as f:
+            f.write(files[lastindex]+"\n")
+        files.pop(lastindex)
+        print(value)
+    play()
 
 root = tk.Tk()
 img = Image.open("colorspace.png").resize((640,640))
@@ -10,33 +42,6 @@ img_tk = ImageTk.PhotoImage(img)
 label = tk.Label(root, image = img_tk)
 label.pack()
 root.resizable(False, False)
-
-
-_, _, files = next(walk("recordings"))
-
-def play():
-    if len(files):
-        index = randrange(len(files))
-        with open("played", "a+") as f:
-            played = list(map(lambda x: x.strip(), f.readlines()))
-            while files[index] in played:
-                index = randrange(len(files))
-            f.write(files[index]+"\n")
-        playsound("recordings/"+files[index])
-        files.pop(index)
-    else:
-        print("All files played.")
-
-initial = True
-def getcolor(event):
-    global initial
-    if not initial:
-        value = img.getpixel((event.x, event.y))
-        print(value)
-    else:
-        initial = False
-    play()
-
 root.bind("<Button 1>", getcolor)
 
 root.mainloop()
