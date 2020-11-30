0c0cb20f251fe373a6bc4f1219e313a9ad36c15f
Into Almiala <into.almiala@gmail.com>
2020-11-30 23:28:26 +0200

    change from playlist to played, remove generate_playlist.py, and finish basic functionality (also update .gitignore for vim)

--- generate_playlist.py
+++
@@ -1,7 +0,0 @@
-from os import walk
-
-_, _, files = next(walk("recordings"))
-
-with open("playlist", "w") as playlist:
-    for file in files:
-        playlist.write(file+"\n")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from os import walk
+from playsound import playsound
+from random import randrange
 
 root = tk.Tk()
 img = Image.open("colorspace.png").resize((640,640))
@@ -8,10 +11,32 @@ label = tk.Label(root, image = img_tk)
 label.pack()
 root.resizable(False, False)
 
-def getcolor(event):
-    value = img.getpixel((event.x, event.y))
-    print(value)
 
-root.bind("<Button 1>", getorigin)
+_, _, files = next(walk("recordings"))
+
+def play():
+    if len(files):
+        index = randrange(len(files))
+        with open("played", "a+") as f:
+            played = list(map(lambda x: x.strip(), f.readlines()))
+            while files[index] in played:
+                index = randrange(len(files))
+            f.write(files[index]+"\n")
+        playsound("recordings/"+files[index])
+        files.pop(index)
+    else:
+        print("All files played.")
+
+initial = True
+def getcolor(event):
+    global initial
+    if not initial:
+        value = img.getpixel((event.x, event.y))
+        print(value)
+    else:
+        initial = False
+    play()
+
+root.bind("<Button 1>", getcolor)
 
 root.mainloop()
