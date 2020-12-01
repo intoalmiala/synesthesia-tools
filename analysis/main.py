5148877fadad94f9ade37a8db3433911ca69ddf2
Into Almiala <into.almiala@gmail.com>
2020-12-01 18:21:31 +0200

    update main.py to work with n clusters

--- analysis/main.py
+++ analysis/main.py
@@ -1,20 +1,28 @@
+import json
+import sys
+from functools import reduce
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import numpy as np
 
-# dataset creation
-from sklearn import datasets
-iris = datasets.load_iris()
-X = iris.data[:, :3]
+INPUT_FILE = sys.argv[1]
+N_CLUSTERS = int(sys.argv[2])
 
-kmeans = KMeans(n_clusters = 3)
+# Read data from input file
+with open(INPUT_FILE) as f:
+    X = np.array(reduce(lambda x, y: x+y, json.load(f).values(), []))
+
+kmeans = KMeans(n_clusters = N_CLUSTERS)
 kmeans.fit(X)
 
-# plot drawing
+cluster_colors = kmeans.cluster_centers_/255
+
+# Plot drawing
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-axes = plt.axes(projection = "3d")
 
-axes.scatter(X[:,0], X[:,1], X[:,2], c = kmeans.labels_)
+ax = plt.axes(projection = "3d")
+ax.scatter(X[:,0], X[:,1], X[:,2], c = cluster_colors[kmeans.labels_])
 
 plt.show()
