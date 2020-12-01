f4257c45005a2ec666471fd0caa05eebdaeba3bc
Into Almiala <into.almiala@gmail.com>
2020-12-01 14:22:35 +0200

    reorganize

---
+++ analysis/main.py
@@ -0,0 +1,22 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+# dataset creation
+from sklearn import datasets
+iris = datasets.load_iris()
+X = iris.data[:, :3]
+Y = iris.target
+
+kmeans = KMeans(n_clusters = 3)
+kmeans.fit(X)
+
+# plot drawing
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+axes = plt.axes(projection = "3d")
+
+axes.scatter(X[:,0], X[:,1], X[:,2], c = kmeans.labels_)
+
+plt.show()
