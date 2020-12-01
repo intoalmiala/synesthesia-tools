69562c54c9c9edb44f9a72bbb984987086bbab3d
Into Almiala <into.almiala@gmail.com>
2020-12-01 21:25:45 +0200

    clean up code in analysis directory

--- analysis/nclusters.py
+++ analysis/nclusters.py
@@ -1,26 +1,30 @@
 import json
 import sys
 from functools import reduce
-from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.cluster import KMeans
 
+# Command line arguments
 INPUT_FILE = sys.argv[1]
 N_CLUSTERS = int(sys.argv[2])
+
 
 # Read data from input file
 with open(INPUT_FILE) as f:
     X = np.array(reduce(lambda x, y: x+y, json.load(f).values(), []))
 
+
+# Calculate clusters using k-means
 kmeans = KMeans(n_clusters = N_CLUSTERS)
 kmeans.fit(X)
 
-cluster_colors = kmeans.cluster_centers_/255
 
-# Plot drawing
+# Plot clusters
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
+cluster_colors = kmeans.cluster_centers_/255
 
+fig = plt.figure()
 ax = plt.axes(projection = "3d")
 ax.scatter(X[:,0], X[:,1], X[:,2], c = cluster_colors[kmeans.labels_])
 
--- analysis/silhouette.py
+++ analysis/silhouette.py
@@ -1,54 +1,59 @@
-import json
 import sys
+import json
 from functools import reduce
+import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-import numpy as np
+import matplotlib.pyplot as plt
 
+# Command line arguments
 INPUT_FILE = sys.argv[1]
-#OUTPUT_FILE = sys.argv[2]
+MAX_N_CLUSTERS = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 
+
+# Read data from input file
 with open(INPUT_FILE) as f:
     X = np.array(reduce(lambda x, y: x+y, json.load(f).values(), []))
 
-colors = np.array([tuple(map(lambda x: x/255, X[i])) for i in range(len(X))])
 
-max_n_clusters = 10
-scores = [0 for _ in range(max_n_clusters+1)]
-for n_clusters in range(2,max_n_clusters+1):
-    kmeans = KMeans(n_clusters = n_clusters)
-    prediction = kmeans.fit_predict(X)
-    scores[n_clusters] = silhouette_score(X, prediction)
+# Calculate clusters using k-means and decide cluster amount using silhouette
+scores = []
+kmeans = []
+for i in range(MAX_N_CLUSTERS-1):
+    n_clusters = i + 2
+    kmeans.append(KMeans(n_clusters = n_clusters))
+    prediction = kmeans[i].fit_predict(X)
+    scores.append(silhouette_score(X, prediction))
     print(f"For {n_clusters} clusters got a score of {scores[n_clusters]}.")
 
 best_score = 0
-best_n = 0
-for n, score in enumerate(scores):
+best_i = 0
+for i, score in enumerate(scores):
     if score > best_score:
         best_score = score
-        best_n = n
+        best_i = i
 
-print(f"Best cluster amount was {best_n} with a score of {best_score}.")
+print(f"Best cluster amount was {best_i + 2} with a score of {best_score}.")
 
-kmeans = KMeans(n_clusters = best_n)
-kmeans.fit(X)
+model = kmeans[best_i]
 
+
+# Plot clusters
+fig = plt.figure()
 cluster_colors = kmeans.cluster_centers_/255
 
-# Plot drawing
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-
-# For plotting only cluster colors
+# For plotting only cluster colors #
 ax = plt.axes(projection = "3d")
 ax.scatter(X[:,0], X[:,1], X[:,2], c = cluster_colors[kmeans.labels_])
+# -------------------------------- #
 
-# For plotting cluster colors and true colors side by side
+# For plotting cluster colors and true colors side by side #
 #ax1 = fig.add_subplot(1, 2, 1, projection = "3d")
 #ax1.scatter(X[:,0], X[:,1], X[:,2], c = cluster_colors[kmeans.labels_])
 #
+#colors = np.array([tuple(map(lambda x: x/255, X[i])) for i in range(len(X))])
 #ax2 = fig.add_subplot(1, 2, 2, projection = "3d")
 #ax2.scatter(X[:,0], X[:,1], X[:,2], c = colors)
+# -------------------------------------------------------- #
 
 plt.show()
