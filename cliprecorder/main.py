e5990c0548df9431e1135863f583f17122dd39ce
Into Almiala <into.almiala@gmail.com>
2020-12-04 18:57:58 +0200

    add basic recording software

--- cliprecorder/main.py
+++ cliprecorder/main.py
@@ -1,10 +1,10 @@
 # This program uses code from: https://python-sounddevice.readthedocs.io/en/0.4.1/examples.html#recording-with-arbitrary-duration
 
-import queue
 import sys
 
 import sounddevice as sd
 import soundfile as sf
+from playsound import playsound
 import numpy
 import tkinter as tk
 
@@ -19,57 +19,78 @@ with open(INPUT_FILE) as f:
     words = list(map(lambda x: x.strip(), f.readlines()))
 
 
-# Initialize queue for audio recording
-q = queue.Queue()
-
-
 # Audio numbers
 SAMPLERATE = 44100
 CHANNELS = 1
+BLOCKSIZE = 512
 
+
+word_index = 0
+word_text = words[0]
+filepath = f"{OUTPUT_DIRECTORY}/{word_text}.wav"
 
 recording = False
 stream = None
+soundfile = None
 
 
-# Function for audio processing
+# Functions for recording and playing audio
 def callback(indata, frames, time, status):
     if status:
         print(status, file=sys.stderr)
-    q.put(indata.copy())
+    soundfile.write(indata.copy())
 
-
-# Function for recording audio
-def record():
-    global recording, stream
+def start_recording():
+    global recording, stream, soundfile
     if not recording:
         recording = True
-        stream = sd.InputStream(samplerate=SAMPLERATE, channels=CHANNELS, callback=callback)
+        stream = sd.InputStream(samplerate=SAMPLERATE, blocksize=BLOCKSIZE, channels=CHANNELS, callback=callback)
+        soundfile = sf.SoundFile(filepath, "w", SAMPLERATE, CHANNELS)
         stream.start()
 
-def stop():
-    global recording, stream
+def stop_recording():
+    global recording
     if recording:
         recording = False
         stream.stop()
         stream.close()
-        with sf.SoundFile("test.wav", "w", SAMPLERATE, CHANNELS) as f:
-            while not q.empty():
-                f.write(q.get())
+        soundfile.close()
 
+def play():
+    playsound(filepath)
+
+
+# Function for getting next word
+def next_word():
+    global word_index, word_text, filepath
+    word_index += 1
+    if word_index == len(words):
+        word_label.config(text="ALL WORDS RECORDED")
+        record_button.pack_forget()
+        stop_button.pack_forget()
+        play_button.pack_forget()
+    elif word_index > len(words):
+        root.destroy()
+    else:
+        word_text = words[word_index]
+        filepath = f"{OUTPUT_DIRECTORY}/{word_text}.wav"
+        word_label.config(text=word_text)
 
 
 # Configure window
 root = tk.Tk()
-play = tk.Button(text="start", command=record)
-stop = tk.Button(text="stop", command=stop)
-play.pack()
-stop.pack()
+
+word_label = tk.Label(root, text=word_text)
+record_button = tk.Button(text="start", command=start_recording)
+stop_button = tk.Button(text="stop", command=stop_recording)
+play_button = tk.Button(text="play", command=play)
+next_button = tk.Button(text="next", command=next_word)
+
+word_label.pack()
+record_button.pack()
+stop_button.pack()
+play_button.pack()
+next_button.pack()
+
 root.mainloop()
-
-
-for word in words:
-    filename = f"{OUTPUT_DIRECTORY}/{word}.wav"
-
-
 
